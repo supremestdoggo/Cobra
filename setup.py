2adb7a8ef4083906eb0e79626040d7ef8f3025d0
supremestdoggo <83146042+supremestdoggo@users.noreply.github.com>
2021-08-28 20:44:04 -0400

    Update setup.py

---
+++ setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="cobra_opt",
+    version="0.0.1",
+    description="An optimizer for Python code.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/supremestdoggo/cobra",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="src"),
+    python_requires=">=3.8",
+)
